f23e2be8d56211a787da4ca039d85caf7f158918
Santiago M.A <santiagomartinagra@gmail.com>
2017-07-05 22:55:09 +0200

    initial version

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Santiago M.A. on 05/07/2017.
+ */
+require('dotenv').config();
+var hue = require("node-hue-api"),
+    HueApi = hue.HueApi,
+	state = hue.lightState.create(),
+	target = process.env.TARGET_ID,
+	api = new HueApi(process.env.HOST, process.env.USER);
+
+var dash_button = require('node-dash-button'),
+	dash = dash_button(process.env.DASH_BUTTON, null, null, 'all');
+
+dash.on('detected', function () {
+	api.lightStatus(target)
+		.then(function (res) {
+			if(res.state.on){
+				api.setLightState(target, state.off());
+			}
+			else{
+				api.setLightState(target, state.on());
+			}
+		})
+		.done();
+});
